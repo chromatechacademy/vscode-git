856b5d7d2ab357166a5d7147be66741170d067d9
Buzjaka <marina.dzhadzha@gmail.com>
2024-02-04 20:03:39 -0500

    second commit

--- src/test/java/com/example/NewJavaClass.java
+++ src/test/java/com/example/NewJavaClass.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class NewJavaClass {
     public static void main(String[] args) {
-        System.out.println("LETS GET THIS PROJECT STARTED");
+        System.out.println("I'm tired of this class");
+        System.out.println("Bye!");
     }
 }
