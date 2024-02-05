908581fea9e94cbc5cdc05e99dd896afef8accf5
chromatechacademy <team@chromatechacademy.com>
2024-02-04 20:07:11 -0500

    making more changes and ignoring maryna

--- src/test/java/com/example/NewJavaClass.java
+++ src/test/java/com/example/NewJavaClass.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class NewJavaClass {
     public static void main(String[] args) {
-        System.out.println("LETS GET THIS PROJECT STARTED");
+        System.out.println("LETS GET THIS PROJECT STARTED ALREADY I DONT LIKE WORKING WITH MARYNA");
+        System.out.println("JUSTICE LEAGUE IS AWESOME");
+        System.out.println("I AM BEING A BAD TEAM PLAYER");
     }
 }
